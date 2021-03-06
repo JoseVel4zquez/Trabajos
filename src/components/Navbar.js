890a540569d4a586059ecea489c7361d8e4c8f3d
Leyville <jlanconap955@icloud.com>
2021-03-06 01:05:37 -0600

    creacion del menu responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,1 +1,38 @@
+
 import React from 'react'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faAlignJustify} from '@fortawesome/free-solid-svg-icons'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import  './functions/NavBarFunction.js'
+import './css/Navbar.css'
+import Logo from '../images/logo.png'
+
+
+export default function Navbar(){
+    
+        return(
+            <header>
+                <nav>
+                    <div className="logo">
+                        <img src={Logo} alt=''/>
+                    </div>
+                    <div className="icono" id="icono">
+                        <span><FontAwesomeIcon icon={faAlignJustify}/></span>
+                    </div>
+                    <div className="enlaces uno" id='enlaces'>
+                        <Router>
+                            <Link to='/' className="active">Home</Link>
+                            <Link to='/'>Acerca de</Link>
+                            <Link to='/'>Contacto</Link>
+                        </Router>
+                    </div>
+
+                </nav>
+                <div class="textos">
+                    <h1>Trabajos en general</h1>
+                    <h2>Las mejores instalaciones </h2>
+                </div>
+            </header>
+        )
+    
+}
