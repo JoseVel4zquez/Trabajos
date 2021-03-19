fcbdcca348a200c50d0c27a920aece23d7b24e96
Leyville <jlanconap955@icloud.com>
2021-03-18 22:53:18 -0600

    correccion de tarjetas y responsive

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,43 +1,65 @@
-import React from 'react'
-import Card from './Card'
-import house from '../images/house.jpg'
+import React from "react";
+import Card from "./Card";
+import house from "../images/house.jpg";
+import "../components/css/home.css";
 
 const cards = [
-    {
-        id: 1,
-        title: 'Trabajos en general',
-        image: house,
-        url: 'https://kstech.club',
-        //text: 'Esto es una prueba chida xd'
-    },
-    {
-        id: 2,
-        title: 'Titulo',
-        image: house,
-        url: 'https://kstech.club'
-    },
-    {
-        id: 3,
-        title: 'Titulo',
-        image: house,
-        url: 'https://kstech.club'
-    }
-]
+  {
+    id: 1,
+    title: "Trabajos en general",
+    image: house,
+    url: "https://kstech.club",
+    //text: 'Esto es una prueba chida xd'
+  },
+  {
+    id: 2,
+    title: "Titulo",
+    image: house,
+    url: "https://kstech.club",
+  },
+  {
+    id: 3,
+    title: "Titulo",
+    image: house,
+    url: "https://kstech.club",
+  },
+  {
+    id: 4,
+    title: "Titulo",
+    image: house,
+    url: "https://kstech.club",
+  },
+  {
+    id: 5,
+    title: "Titulo",
+    image: house,
+    url: "https://kstech.club",
+  },
+  {
+    id: 6,
+    title: "Marcas",
+    image: house,
+    url: "https://kstech.club",
+  },
+];
 
 function Cards() {
-    return (
-        <div className="container d-flex justify-content-center align-items-center h-100">
-            <div className="row">
-               {
-                   cards.map(card =>(
-                       <div className="col-md-4" key={card.id}>
-                           <Card title={card.title} imageSrc={card.image} url={card.url} text={card.text}/> 
-                       </div>
-                   ))
-               }
-            </div>
-        </div>
-    )
+  return (
+    <div className="container-fluid d-flex justify-content-center align-items-center Home">
+      <div className="row mt-5 w-50">
+        {cards.map((card) => (
+          <div className="mt-5 mb-5 col-md-4" key={card.id}>
+            <Card
+              title={card.title}
+              imageSrc={card.image}
+              url={card.url}
+              text={card.text}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Cards
+export default Cards;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,22 +1,20 @@
-import React from 'react'
-import Navbar from './Navbar'
-import Nosotros from './Nosotros'
-import Footer from './Footer'
-import Cards from './Cards'
-import './css/home.css'
+import React from "react";
+import Navbar from "./Navbar";
+import Nosotros from "./Nosotros";
+import Footer from "./Footer";
+import Cards from "./Cards";
+import "./css/home.css";
 
-import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
-export default function Home (){
-    return(
-        <div>
-            <Navbar/>
-            <Nosotros/>
-            <div className="Home">
-                <Cards/>
-            </div>
-            <Footer/>
-        </div>
-        
-    )
+export default function Home() {
+  return (
+    <div>
+      <Navbar />
+      <Nosotros />
+      <Cards />
+
+      <Footer />
+    </div>
+  );
 }
