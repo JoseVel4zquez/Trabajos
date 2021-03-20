979b720de7ea743a2160532497e075f6e9e43d3c
Leyville <jlanconap955@icloud.com>
2021-03-20 12:00:27 -0600

    correccion

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,41 +1,38 @@
-
-import React from 'react'
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faAlignJustify} from '@fortawesome/free-solid-svg-icons'
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAlignJustify } from "@fortawesome/free-solid-svg-icons";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import  './functions/NavBarFunction.js'
-import './css/Navbar.css'
-import Logo from '../images/logo.png'
-import Nosotros from './Nosotros'
+import "./functions/NavBarFunction.js";
+import "./css/Navbar.css";
+import Logo from "../images/logo.png";
+import Nosotros from "./Nosotros";
 
-
-export default function Navbar(){
-    
-        return(
-            <header>
-                <nav>
-                    <div className="logo">
-                        <img src={Logo} alt=''/>
-                    </div>
-                    <div className="icono" id="icono">
-                        <span><FontAwesomeIcon icon={faAlignJustify}/></span>
-                    </div>
-                    <div className="enlaces uno" id='enlaces'>
-                        <Router>
-                            <Link to='/' className="active">Home</Link>
-                            <Link to='/'>Acerca de</Link>
-                            <Link to='/'>Contacto</Link>
-                        </Router>
-                    </div>
-
-                </nav>
-                <div className="textos">
-                    <h1>Trabajos en general</h1>
-                    <h2>Las mejores instalaciones </h2>
-                </div>
-            </header>
-            
-            
-        )
-    
+export default function Navbar() {
+  return (
+    <header>
+      <nav>
+        <div className="logo">
+          <img src={Logo} alt="" />
+        </div>
+        <div className="icono" id="icono">
+          <span>
+            <FontAwesomeIcon icon={faAlignJustify} />
+          </span>
+        </div>
+        <div className="enlaces uno" id="enlaces">
+          <Router>
+            <Link to="/" className="active">
+              Home
+            </Link>
+            <Link to="/">Acerca de</Link>
+            <Link to="/">Contacto</Link>
+          </Router>
+        </div>
+      </nav>
+      <div className="textos">
+        <h1>Trabajos en general</h1>
+        <h2>Las mejores instalaciones </h2>
+      </div>
+    </header>
+  );
 }
---
+++ src/components/Router.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import Home from "./Home";
+import Productos from "./Productos";
+
+export default function Router() {
+  return (
+    <BrowserRouter>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/home" component={Home} />
+      <Route exact path="/product" component={Productos} />
+    </BrowserRouter>
+  );
+}
